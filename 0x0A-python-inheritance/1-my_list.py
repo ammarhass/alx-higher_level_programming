7dc48160eb957e7f0995da4e8c5d6f370d557602
ammarhass <133575804+ammarhass@users.noreply.github.com>
2023-10-27 22:31:22 +0200

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""MyList class that inherits from list"""
-
-
-class MyList(list):
-    """ print the list in an ascending order"""
-
-    def print_sorted(self):
-        print(sorted(self))
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""BaseGeometry class"""
-
-
-class BaseGeometry:
-    """Basegeometry"""
-    pass
