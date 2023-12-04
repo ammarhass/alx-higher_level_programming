ece898feb06a12677863bb1cdabe08866bc7ec0e
ammar <ammar.hass94@gmail.com>
2023-12-04 11:51:43 +0200

    Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,12 +9,13 @@ class BaseGeometry
 class BaseGeometry:
     """BaseGeometry class"""
 
-
+    @classmethod
     def area(self):
         """function to get the area"""
 
         raise Exception("area() is not implemented")
 
+    @classmethod
     def integer_validator(self, name, value):
         """function to check the validation of an integer"""
 
