23764a977f40b9c0e1558de3433956fd0d5c5ba5
ammar <ammar.hass94@gmail.com>
2023-12-04 12:37:41 +0200

    Task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" class Rectangle """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """intializa a Rectangle"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        """represent a Rectangle"""
+
+        s = "[" + str(self.__class__.name) + "] "
+        s += str(self.__width) + "/" + str(self.__height)
+        return ss
