d6f3dae45538d399873db084f95236c107d177d2
ammar <ammar.hass94@gmail.com>
2023-12-05 11:08:24 +0200

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     "function that reads a text file"
     with open(filename, encoding="utf-8") as file:
-        print(f.read(), end="")
+        print(file.read(), end="")
