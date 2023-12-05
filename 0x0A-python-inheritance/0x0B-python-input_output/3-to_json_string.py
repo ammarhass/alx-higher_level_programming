73b355b9e41741bdbc46d2dacbe8d642494ae5ef
ammar <ammar.hass94@gmail.com>
2023-12-05 10:49:18 +0200

    updating

--- 0x0A-python-inheritance/0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import json
-
-def to_json_string(my_obj):
-    """function that return the JSON representation of an object"""
-    return json.dumps(my_obj)
--- 0x0A-python-inheritance/0x0B-python-input_output/6-load_from_json_file.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import json
-def load_from_json_file(filename):
-    """function that creates an Object from a "JSON file" """
-    with open(filename) as file:
-        return json.load(f)
