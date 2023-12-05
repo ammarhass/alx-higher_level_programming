aabc98e10b295e5e35d49682fd451fd1eeb9a27d
ammar <ammar.hass94@gmail.com>
2023-12-05 11:10:31 +0200

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""define a function"""
+
+
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
     with open(filename, "w", encoding="utf-8") as file:
