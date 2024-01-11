9e0971ae90f73cb487638dfd7627d80f1d3d5a32
ammar <ammar.hass94@gmail.com>
2024-01-11 14:31:54 +0200

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,6 +23,5 @@ class Student:
 
     def reload_from_json(self, json):
         """function to replace all attribute of student instance"""
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        for k, v in json.items():
+            setattr(self, k, v)
