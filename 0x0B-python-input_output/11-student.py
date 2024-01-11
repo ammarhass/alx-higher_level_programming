866e724e8baf49bf59c5484a35bf900fbae71698
ammar <ammar.hass94@gmail.com>
2024-01-11 14:10:02 +0200

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" a student class """
+
+
+class Student:
+    """ define a Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """intialize a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary representation of an object"""
+        if attrs is None:
+            return self.__dict__
+        _dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                _dict[key] = value
+        return _dict
+
+    def reload_from_json(self, json):
+        """function to replace all attribute of student instance"""
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
